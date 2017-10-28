c7d34096cabe202126b3b43eb3ef9ff3e844af58
Eugene M. <ujeen1980@gmail.com>
2017-10-29 00:51:02 +0200

    Added Calculation Date and Total wage count

--- ST-TestTask/ST-TestTask/Form1.Designer.cs
+++ ST-TestTask/ST-TestTask/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.Employees = new System.Windows.Forms.DataGridView();
-            this.label1 = new System.Windows.Forms.Label();
             this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.EmpName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Lastname = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -39,6 +38,11 @@
             this.Group = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Head = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Subordinates = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.Employees)).BeginInit();
             this.SuspendLayout();
             // 
@@ -59,15 +63,6 @@
             this.Employees.Name = "Employees";
             this.Employees.Size = new System.Drawing.Size(844, 193);
             this.Employees.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Employees:";
             // 
             // id
             // 
@@ -123,13 +118,63 @@
             this.Subordinates.Name = "Subordinates";
             this.Subordinates.Width = 200;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employees:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Calculation Date:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(106, 9);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Total wage:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(81, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(22, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "0.0";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(862, 432);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Employees);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "ST-TestTask";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -152,6 +197,10 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn Group;
         private System.Windows.Forms.DataGridViewTextBoxColumn Head;
         private System.Windows.Forms.DataGridViewTextBoxColumn Subordinates;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- ST-TestTask/ST-TestTask/Form1.cs
+++ ST-TestTask/ST-TestTask/Form1.cs
@@ -7,14 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace ST_TestTask
 {
     public partial class Form1 : Form
     {
-        
-        Main main = new Main();
-        
+        private Main main = new Main();
+        private DateTime? _dt;
+        private double _total;
 
         public Form1()
         {
@@ -24,29 +23,56 @@ namespace ST_TestTask
         private void Form1_Load(object sender, EventArgs e)
         {
            FillTable();
+           CountTotal();
         }
 
-        private void FillTable()
-        {
-            DateTime? dt = null;
+		private void FillTable()
+		{
+			foreach (Person person in main.personnel)
+			{
+				Employees.Rows.Add
+					(
+					person.ID,
+					person.NAME,
+					person.LASTNAME,
+					person.STARTDATE.ToString("d"),
+					person.WAGERATE,
+					String.Format("{0,12:000.00}", main.CalculateWage(person.ID, person.STARTDATE, person.WAGERATE, person.GROUP, _dt)),
+					person.GROUP,
+					main.GetBossName(person.ID),
+					main.GetSubordinatesNames(person.ID)
+					);
+			}
 
-            foreach (Person person in main.personnel)
+		}
+
+        private void CountTotal()
+        {
+            try
             {
-                Employees.Rows.Add
-                    (
-                    person.ID,
-                    person.NAME,
-                    person.LASTNAME,
-                    person.STARTDATE.ToString("d"),
-                    person.WAGERATE,
-                    String.Format("{0,12:000.00}", main.CalculateWage(person.ID, person.STARTDATE, person.WAGERATE, person.GROUP, dt)),
-                    person.GROUP,
-                    main.GetBossName(person.ID),
-                    main.GetSubordinatesNames(person.ID)
-                    );
+                foreach (DataGridViewRow dgvr in Employees.Rows)
+                {
+                    _total += Double.Parse(dgvr.Cells[5].Value.ToString());
+                }
+                
+            }
+            catch (Exception e)
+            {
 
             }
+            finally
+            {
+                label4.Text = _total.ToString();
+            }
+          
+        }
 
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            Employees.Rows.Clear();
+            Employees.Refresh();
+            _dt = dateTimePicker1.Value;
+            FillTable();
         }
     }
 }
